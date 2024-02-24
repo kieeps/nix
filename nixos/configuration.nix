51df4d0db2a3bb1b1afae30f9b022c820c320d08
kieeps <kieeps@kieeps.com>
2024-02-24 18:17:11 +0100

    mor config

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -172,7 +172,7 @@
   # FIXME: Add the rest of your current configuration
 
   # TODO: Set your hostname
-  networking.hostName = "your-hostname";
+  networking.hostName = "lappen";
 
   # TODO: This is just an example, be sure to use whatever bootloader you prefer
   boot.loader.systemd-boot.enable = true;
